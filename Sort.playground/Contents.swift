103c1a76177f12ad5ff36e8a15601b238ba858b7
Swift Funcional <info@swiftfuncional.com>
2017-04-04 09:14:25 +0200

    Sorted returns a new array

--- Sort.playground/Contents.swift
+++ Sort.playground/Contents.swift
@@ -2,8 +2,8 @@ let array = Array(1...4)
 
 print(array)
 
-array.sorted { number1, number2 in
+let sortedArray = array.sorted { number1, number2 in
 	number1 > number2
 }
 
-print(array)
+print(sortedArray)
